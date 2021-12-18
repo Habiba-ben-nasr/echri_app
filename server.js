d4beb37e02db78d001b7548001795f1e6a393fc9
Habiba-Ben-Nasr <bennasrhabiba@gmail.com>
2021-12-18 12:22:30 +0100

    Create  database using mongoose (mongoDB)

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config({path:"./config/.env"});
 
-const express=require("express")
+const express=require("express");
+const connectDB = require('./config/connectDB');
 
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
+connectDB();
 app.listen(process.env.PORT , (err)=>{
     err?  console.log("Server connection failed",err)
-    : console.log("Server connected");
+    : console.log(`💻 is connected on 🚪 ${process.env.PORT}`);
 })
